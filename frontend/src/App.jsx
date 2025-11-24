c25df4732c6a676aaf6e8840ea4910a5c2d325b5
RAJKANYA27 <rajkanyapatil47@gmail.com>
2025-11-25 05:03:59 +0530

    Create App.jsx

---
+++ frontend/src/App.jsx
@@ -0,0 +1,14 @@
+import './index.css';
+import MapView from './components/MapView';
+
+function App() {
+  return (
+    <div className="h-screen w-screen flex">
+      <div className="flex-1">
+        <MapView />
+      </div>
+    </div>
+  );
+}
+
+export default App;
